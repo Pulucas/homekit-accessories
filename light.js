68a9e4fb0acd3f8453443f1adec64698855d0b41
Lucas <40408423+Pulucas@users.noreply.github.com>
2024-06-12 21:53:25 -0400

    first commit

---
+++ light.js
@@ -0,0 +1,59 @@
+const hap = require('hap-nodejs');
+const Accessory = hap.Accessory;
+const Characteristic = hap.Characteristic;
+const CharacteristicEventTypes = hap.CharacteristicEventTypes;
+const Service = hap.Service;
+
+const accessoryUuid = hap.uuid.generate('art.lucash.myLight');
+const myLight = new Accessory('My Light', accessoryUuid);
+const myLightService = new Service.Lightbulb("My Light");
+
+let currentLightState = false;
+let currentBrightnessLevel = 100;
+
+const onCharacteristic = myLightService.getCharacteristic(Characteristic.On);
+const brightnessCharacteristic = myLightService.getCharacteristic(Characteristic.Brightness);
+
+onCharacteristic.on(CharacteristicEventTypes.GET, callback => {
+  console.log("Queried current light state:", currentLightState);
+  callback(undefined, currentLightState);
+});
+
+onCharacteristic.on(CharacteristicEventTypes.SET, (value, callback) => {
+  console.log("Settings light state to:", value);
+  currentLightState = value;
+  callback();
+});
+
+brightnessCharacteristic.on(CharacteristicEventTypes.GET, callback => {
+  console.log("Queried current brightness level:", currentBrightnessLevel);
+  callback(undefined, currentBrightnessLevel);
+});
+
+brightnessCharacteristic.on(CharacteristicEventTypes.SET, (value, callback) => {
+  console.log("Setting brightness level to:", value);
+  currentBrightnessLevel = value;
+  callback();
+});
+
+myLight.addService(myLightService);
+
+// Generates a random pincode on startup
+const randomCode = parseInt(Math.random() * 100000000);
+const pincode = randomCode.toString().split("");
+pincode.splice(3, 0, "-");
+pincode.splice(6, 0, "-");
+console.log("pinode is " + pincode.join(""));
+
+myLight.publish({
+  username: '17:51:07:F4:BC:8A',
+  pincode: pincode.join(""),
+  category: Accessory.Categories.LIGHTBULB
+});
+
+console.log("Accessory setup finished");
+
+// function doSomething(state) {
+//   if (state) console.log("Light is on");
+//   else console.log("Light is off");
+// };
